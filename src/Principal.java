b52a6c406a186fb79e7cb86e912fde348249aaf5
Juan MIguel <jumime.hotmail.com>
2024-04-18 19:52:21 -0500

    Nueva actualización

--- src/Principal.java
+++ src/Principal.java
@@ -2,5 +2,6 @@ public class Principal {
     public static void main(String[] args) {
         System.out.println("Primer prueba git");
         System.out.println("En desarrollo");
+        
     }
 }
